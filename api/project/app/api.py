9748b5a8625cd93479c2672fe275612668c7ba4e
mrpoopy <junk@krenitsky.org>
2022-10-21 19:52:02 -0400

    api auth is almost there

--- api/project/app/api.py
+++ api/project/app/api.py
@@ -1,36 +1,43 @@
-from flask import jsonify, Blueprint, make_response
+from flask import jsonify, Blueprint, make_response, request
 from flask import current_app as app
 from datetime import datetime as dt
 from datetime import timedelta
 from project.models import db, Temps
-
+import json
+from werkzeug.security import generate_password_hash, check_password_hash
+import base64
 
 
 ## Blueprint Config
 api_bp = Blueprint('api_bp', __name__)
 
 
-@api_bp.route('/hi', endpoint='say_hi', methods=['GET'])
-def say_hi():
-    return jsonify("hellow")
+# @api_bp.route('/hi', endpoint='say_hi', methods=['GET'])
+# def say_hi():
+#     return jsonify("hellow")
 
 
-@api_bp.route('/make_temp_point', endpoint='make_temp_point', methods=['POST'])
+@api_bp.route('/post', endpoint='post', methods=['POST'])
 def make_temp_point():
-    data = Temps.query.all()    #clear temperature rows older than needed time window
-    for row in data:
-        now = dt.now()
-        if (now - row.time) > timedelta(days=8):
-            db.session.delete(row)
-            db.session.commit()
-    temp_h = sense.get_temperature_from_humidity()
-    temp_p = sense.get_temperature_from_pressure()
-    temp_mean = (temp_h + temp_p) / 2
-    time = dt.now()
-    new_t_point = Temps(temperature=temp_mean, time=time)
-    db.session.add(new_t_point)
-    db.session.commit()
-    return {
-        "time": time,
-        "temperature": temp_mean
-    }
+    auth_cyph = request.headers['Authorization'][6:]
+    decyph = base64.b64decode(auth_cyph).decode("utf-8").split(":")
+    with open('auth.txt') as file:
+        pw_hash = file.read()
+    if decyph[0] != 'myshkins' or not check_password_hash(pw_hash, decyph[1]):
+        return jsonify({'error':'404'})
+    else:
+        print("you're in")
+    j = request.get_json()
+    data = json.loads(j)
+    temp = data['temperature']
+    time = dt.strptime(data['time'], '%Y-%m-%d %H:%M:%S.%f')
+    print(time)
+    # data = Temps.query.all()    #clear temperature rows older than needed time window
+    # for row in data:
+    #     if (now - row.time) > timedelta(days=8):
+    #         db.session.delete(row)
+    #         db.session.commit()
+    # new_t_point = Temps(temperature=temp_mean, time=time)
+    # db.session.add(new_t_point)
+    # db.session.commit()
+    return make_response()
